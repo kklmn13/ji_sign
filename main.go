d29ec1ff016f633b056c8c23140aeddb8e3cfcfc
kklmn13 <cyan_13@foxmail.com>
2021-07-07 11:28:25 +0800

    去除无用日志

--- main.go
+++ main.go
@@ -19,21 +19,34 @@ func init()  {
 }
 
 func main() {
+	showMsg("启动，开始签到...")
 	signall()
+	showMsg("签到结束，启动定时任务...")
 	c := cron.New()
-	c.AddFunc("0 * * * * ?", func() {
-		timeNow:=time.Now()
-		timeNowStr:=timeNow.Format("2006-01-02 15:04:05")
-		fmt.Print(timeNowStr +": test cron ,every hour \n")
-	})
+	//c.AddFunc("0 * * * * ?", func() {
+	//	timeNow:=time.Now()
+	//	timeNowStr:=timeNow.Format("2006-01-02 15:04:05")
+	//	fmt.Print(timeNowStr +": test cron ,every hour \n")
+	//})
 	c.AddFunc("0 0 9 * * ?", func() {
-		fmt.Print("每天9:00签到")
+		showMsg("到点自动签到...")
+		signall()
+		showMsg("签到结束.")
 		signall()
 	})
 	c.Start()
+	showMsg("定时任务启动完成.")
 	select {}
 }
 
+//消息打印
+func showMsg(a_msg string) {
+	timeNow:=time.Now()
+	timeNowStr:=timeNow.Format("2006-01-02 15:04:05")
+	fmt.Print(timeNowStr + ":" + a_msg + " \n")
+}
+
+//签到所有配置账号
 func signall(){
 	if (len(util.AppConfig.GetString("email1")) != 0) {
 		sign(util.AppConfig.GetString("email1"), util.AppConfig.GetString("passwd1"))
